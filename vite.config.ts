203b7c181b2518d94c3f60cbfedbb6bd0e52ddb5
Marlieghjai <64768483+Marlieghjai@users.noreply.github.com>
2026-02-14 00:04:21 -0500

    Update vite.config.ts
    fixed naming issue

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/early-bird-access/",
+  base: "/-early-bird-access/",
   server: {
     host: "::",
     port: 8080,
